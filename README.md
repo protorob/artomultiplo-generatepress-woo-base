6cfe119cc97769b41d210e7d215d4ffdc835e5a5
Roberto Briceno <hello@artomultiplo.eu>
2020-05-13 11:16:59 +0200

    First Commit

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # artomultiplo-generatepress-woo-base
 My starter child for generatepress woocommerce stores
+
+**This Child Theme is made specifically for [GeneratePress](https://generatepress.com/?ref=185), so you will need to first install that theme.**
+
+This [GeneratePress](https://generatepress.com/?ref=185) Child Theme also asumes that you haeve installed also the [GeneratePress Premium Add-ons](https://generatepress.com/premium/?ref=185).
+
+Even if this is not strictly necessary I think that both this cart and login plugins are really nice and makes the shopping experience a bit better, but it might be a personal opinion. **There are styles in this Child Theme to target those specific plugins**
+
+* For the Login/Signup Popup: [XootiX Login/Signup Popup](https://xootix.com/plugins/easy-login-for-woocommerce/)
+* For the popup cart: [Festi-Team WooCart Pro](http://festi.team/plugins/woocommerce-cart/)
+
+## What is this ChildTheme changing in both GeneratePress and WooCommerce?
+
+Primarily, this child theme modifies the display of the Default Checkout and Cart Pages in order to 
+
+### Credits:
+Illustrations used in Screenshot.png by [Pablo Stanley](http://payforlayers.com/)
---
+++ functions.php
@@ -0,0 +1,120 @@
+<?php
+/**
+ * GeneratePress child theme functions and definitions.
+ *
+ * Add your custom PHP in this file. 
+ * Only edit this file if you have direct access to it on your server (to fix errors if they happen).
+ */
+
+function generatepress_child_enqueue_scripts() {
+	if ( is_rtl() ) {
+		wp_enqueue_style( 'generatepress-rtl', trailingslashit( get_template_directory_uri() ) . 'rtl.css' );
+	}
+}
+
+add_action( 'wp_enqueue_scripts', 'generatepress_child_enqueue_scripts', 100 );
+
+
+// Change Login styles
+/*Change Login Logo*/
+function my_login_logo() { ?>
+    <style type="text/css">
+        #login h1 a, .login h1 a {
+        background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/img/artomultiplo-login-logo.svg);
+        height:150px;
+        width:150px;
+        background-size: 150px 150px;
+        background-repeat: no-repeat;
+        padding-bottom: 0px;
+        }
+    </style>
+<?php }
+add_action( 'login_enqueue_scripts', 'my_login_logo' );
+
+/*Change admin logo link to homepage*/
+function my_login_logo_url() {
+    return home_url();
+}
+add_filter( 'login_headerurl', 'my_login_logo_url' );
+
+/*Change login logo title*/
+function my_login_logo_url_title() {
+    return 'Pasticceria Amodei di Amodei Angelo - Admin Login';
+}
+add_filter( 'login_headertitle', 'my_login_logo_url_title' );
+
+/*Enqueuing an stylesheet only for login*/
+function my_login_stylesheet() {
+    wp_enqueue_style( 'custom-login', get_stylesheet_directory_uri() . '/css/login-styles.css' );
+}
+add_action( 'login_enqueue_scripts', 'my_login_stylesheet' );
+
+
+/* Hide the "description" title in single-product page */
+
+function remove_product_description_heading() {
+	return '';
+}
+
+add_filter( 'woocommerce_product_description_heading', 'remove_product_description_heading' );
+
+// Turn off Ship to a different address checkbox in cart
+// from https://metorik.com/blog/change-woocommerce-ship-to-a-different-address-default	
+
+add_filter( 'woocommerce_ship_to_different_address_checked', '__return_false' );
+
+/*Remove downloads from my_account page*/
+/*https://rudrastyh.com/woocommerce/my-account-menu.html*/
+
+add_filter ( 'woocommerce_account_menu_items', 'misha_remove_my_account_links' );
+function misha_remove_my_account_links( $menu_links ){
+ 
+    unset( $menu_links['downloads'] ); // Addresses
+    return $menu_links;
+ 
+}
+
+/*Move category description after loop*/
+
+add_action('woocommerce_archive_description', 'custom_archive_description', 2 );
+function custom_archive_description(){
+    if( is_product_category() ) :
+        remove_action('woocommerce_archive_description', 'woocommerce_taxonomy_archive_description', 10 );
+        add_action( 'woocommerce_after_main_content', 'woocommerce_taxonomy_archive_description', 5 );
+    endif;
+}
+
+/*Hide flat rati if free shipping is available*/
+
+add_filter( 'woocommerce_package_rates', 'artomultiplo_unset_shipping_when_free_is_available_in_zone', 10, 2 );
+   
+function artomultiplo_unset_shipping_when_free_is_available_in_zone( $rates, $package ) {
+      
+// Only unset rates if free_shipping is available
+if ( isset( $rates['free_shipping:2'] ) ) {
+     unset( $rates['flat_rate:1'] );
+}     
+     
+return $rates;
+  
+}
+
+/*Login Logo and Admin Menu Logo*/
+
+/*Add a logo in admin left menu*/
+
+add_action('admin_menu', 'artomultiplo_admin_menu_logo');
+
+function artomultiplo_admin_menu_logo() {
+    global $menu;
+    $url = '#';
+    $menu[0] = array( __('Artomultiplo'), 'read', $url, 'am-client-site-logo', 'am-client-site-logo');
+}
+
+/*Admin Stylesheet*/
+
+add_action('admin_head', 'artomultiplo_admin_style');
+
+function artomultiplo_admin_style() {
+    echo '<link rel="stylesheet" href="' . get_stylesheet_directory_uri() . '/css/admin_style.css" type="text/css" media="all" />';
+}
